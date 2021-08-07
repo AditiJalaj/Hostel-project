7d5b1f16d89cf5c4a652f0f1fd1895713c6e7406
Chiragmodi01 <75688193+Chiragmodi01@users.noreply.github.com>
2021-08-07 09:24:03 -0700

    resolved: package error

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
-import './App.css';
-import Contact from './components/Contact/Contact';
-import Login from './components/Login/Login';
-import Home from './components/HomePage/Home';
-<<<<<<< HEAD
-import Navbar from './components/HomePage/Navbar/Navbar';
-=======
-import Delhi from './components/Products/Delhi/Delhi';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
-import Kerala from './components/Products/Kerala/Kerala';
-import Mumbai from './components/Products/Mumbai/Mumbai';
-import Rajasthan from './components/Products/Rajasthan/Rajasthan';
-import Navbar from './components/HomePage/Navbar';
-
->>>>>>> 27aef5da202a4d5421592f3a29feda1e5f7b63b9
+import "./App.css";
+import Contact from "./components/Contact/Contact";
+import Login from "./components/Login/Login";
+import Home from "./components/HomePage/Home";
+import Delhi from "./components/Products/Delhi/Delhi";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Kerala from "./components/Products/Kerala/Kerala";
+import Mumbai from "./components/Products/Mumbai/Mumbai";
+import Rajasthan from "./components/Products/Rajasthan/Rajasthan";
+import Navbar from "./components/HomePage/Navbar";
 
 function App() {
   return (
-    
     <div className="App">
-<<<<<<< HEAD
-      <Navbar />
-      <Home />
-=======
-    <Router>
-    <Navbar/>
-      <Switch>
-
-      <Route path='/' exact component={Home}/>
-      <Route path='/delhi' exact component={Delhi}/>
-      <Route path='/kerala' exact component={Kerala}/>
-      <Route path='/mumbai' exact component={Mumbai}/>
-      <Route path='/rajasthan' exact component={Rajasthan}/>
-      <Route exact path="/contact" component={Contact}/>
-    <Route exact path="/login" component={Login}/>
-     
-      </Switch>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/delhi" exact component={Delhi} />
+          <Route path="/kerala" exact component={Kerala} />
+          <Route path="/mumbai" exact component={Mumbai} />
+          <Route path="/rajasthan" exact component={Rajasthan} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
       </Router>
->>>>>>> 27aef5da202a4d5421592f3a29feda1e5f7b63b9
     </div>
-    
   );
 }
 
--- src/components/HomePage/Destinations.js
+++ src/components/HomePage/Destinations.js
@@ -1,127 +1,144 @@
-import React from 'react'
-import './styles/Destinations.css';
-import {
-    BrowserRouter as Router,
-    Link
-  } from "react-router-dom";
+import React from "react";
+import "./styles/Destinations.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-import Delhi from '../Products/Delhi/Delhi';
+import Delhi from "../Products/Delhi/Delhi";
 
 function Destinations() {
+  // const scrollx = () => {
+  //     window[`scrollTo`]({ width: 300, behavior: `smooth`});
+  //     console.log("clicked");
+  // }
 
-    // const scrollx = () => {
-    //     window[`scrollTo`]({ width: 300, behavior: `smooth`});
-    //     console.log("clicked");
-    // }
-
-    return (
-       
-        <div className="destinations-container">
-            <div className="destinations-head">
-                <h2>BRANDNAME DESTINATIONS</h2>
-            </div>
-            <div className="destinations-card">
-                <div className="card card-1">
-<<<<<<< HEAD
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/delhi'> Delhi </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-                </div>
-                <div className="card card-2">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/kerela'> Kerela </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-                </div>
-                <div className="card card-3">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/Mumbai'> Mumbai </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-                </div>
-                <div className="card card-4">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/rajasthan'> Rajasthan </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-=======
-                    <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/268029938.jpg?k=4b142d4f59ce983d1867b826d6620fdc6a2d58ba1ed04c1a9d276d3f5bab9c12&o=&hp=1" alt="destination" className="destination-img" />
-                    <h2 className="destination-name">
-                    <Link to='/delhi'> Delhi </Link>
-                    </h2>
-                </div>
-                <div className="card card-2">
-                    <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/268029938.jpg?k=4b142d4f59ce983d1867b826d6620fdc6a2d58ba1ed04c1a9d276d3f5bab9c12&o=&hp=1" alt="destination" className="destination-img" />
-                    <h2 className="destination-name">
-                    <Link to='/kerala'>  Kerala </Link>
-                   </h2>
-                </div>
-                <div className="card card-3">
-                    <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/268029938.jpg?k=4b142d4f59ce983d1867b826d6620fdc6a2d58ba1ed04c1a9d276d3f5bab9c12&o=&hp=1" alt="destination" className="destination-img" />
-                    <h2 className="destination-name">
-                    <Link to='/mumbai'> Mumbai  </Link>
-                    </h2>
-                </div>
-                <div className="card card-4">
-                    <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/268029938.jpg?k=4b142d4f59ce983d1867b826d6620fdc6a2d58ba1ed04c1a9d276d3f5bab9c12&o=&hp=1" alt="destination" className="destination-img" />
-                    <h2 className="destination-name">
-                    <Link to='/rajasthan'>Rajasthan  </Link></h2>
->>>>>>> 27aef5da202a4d5421592f3a29feda1e5f7b63b9
-                </div>
-                <div className="card card-5">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/leh'> Leh </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-                </div>
-                <div className="card card-6">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/agra'> Agra </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-                </div>
-                <div className="card card-4">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/bir'> Bir </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-                </div>
-                <div className="card card-5">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg" alt="destination" className="destination-img" />
-                    <div className="destination-info">
-                        <h2 className="destination-name">
-                        <Link to='/mussoorie'> Mussoorie </Link>
-                        </h2>
-                        <h4 className="destination-description">of himalayas, lakes and monesteries</h4>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="destinations-container">
+      <div className="destinations-head">
+        <h2>BRANDNAME DESTINATIONS</h2>
+      </div>
+      <div className="destinations-card">
+        <div className="card card-1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/delhi"> Delhi </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
         </div>
-
-        
-    )
+        <div className="card card-2">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/kerela"> Kerela </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
+        </div>
+        <div className="card card-3">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/Mumbai"> Mumbai </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
+        </div>
+        <div className="card card-4">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/rajasthan"> Rajasthan </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
+        </div>
+        <div className="card card-5">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/leh"> Leh </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
+        </div>
+        <div className="card card-6">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/agra"> Agra </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
+        </div>
+        <div className="card card-4">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/bir"> Bir </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
+        </div>
+        <div className="card card-5">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Viewofleh.jpg/349px-Viewofleh.jpg"
+            alt="destination"
+            className="destination-img"
+          />
+          <div className="destination-info">
+            <h2 className="destination-name">
+              <Link to="/mussoorie"> Mussoorie </Link>
+            </h2>
+            <h4 className="destination-description">
+              of himalayas, lakes and monesteries
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Destinations
+export default Destinations;
